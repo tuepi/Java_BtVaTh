82f1d0f1b5e2bf2f81e457ba0492bce554b4ee3c
tuepi <trandinhtue140398@gmail.com>
2022-04-11 01:39:47 +0700

    Xong p2 p3

---
+++ src/ke_thua/point2D_3D/Test2D3D.java
@@ -0,0 +1,11 @@
+package ke_thua.point2D_3D;
+
+public class Test2D3D {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(3.0f, 4.6f);
+        System.out.println(point2D);
+
+        Point3D point3D = new Point3D(3.4f, 4.3f, 1.4f);
+        System.out.println(point3D);
+    }
+}
